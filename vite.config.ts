8bb1bbdcc943d9a51a34bceaafa26f6b2eaea8db
Fengyan Tina <alwarzontina @gmail.com>
2024-10-26 15:37:48 +0200

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/BKC-Web-Dev/',
     build: {
         rollupOptions: {
           output: {
@@ -14,6 +15,6 @@ export default defineConfig({
           }
         }
       },
-    base: '/BKC-Web-Dev/',
+   
   plugins: [react()],
 })
